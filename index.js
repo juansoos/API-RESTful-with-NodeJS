84f0551fe2db1e6878f1d4de947fe4b80d4fcd09
Julian Sotelo <juliansotelo0825@gmail.com>
2017-07-10 17:32:51 -0500

    Implementacion de almacenamiento de datos mediante POST

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+const Product = require('./models/product')
+
 const PORT = process.env.PORT || 3000
 const DB = process.env.MONGODB_URI || 'mongodb://localhost:27017/api-rest'
 
@@ -18,7 +20,19 @@ app.get('/api/product', (req, res) => {})
 // Ruta para consultar un producto
 app.get('/api/product/:productId', (req, res) => {})
 // Ruta para añadir productos
-app.post('/api/product', (req, res) => {})
+app.post('/api/product', (req, res) => {
+  let product = new Product()
+  product.name = req.body.name
+  product.picture = req.body.name
+  product.price = req.body.price
+  product.category = req.body.category
+  product.description = req.body.description
+
+  product.save((err, productStored) => {
+    if (err) res.status(500).send({ message: err })
+    res.status(200).send({ product: productStored })
+  })
+})
 // Ruta para actualizar un producto
 app.put('/api/product/:productId', (req, res) => {})
 // Ruta para borrar un producto
